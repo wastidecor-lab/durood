d7b4cc5ff63ed50198ef7daa8c1f334ab65ed209
Wasti Decor <wastidecor@gmail.com>
2025-08-25 17:34:45 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -65,7 +65,7 @@ export function Header() {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="h-6 w-6 fill-primary">
               <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm-8-88a8,8,0,0,1,8-8,56,56,0,0,1,56,56,8,8,0,0,1-16,0,40,40,0,0,0-40-40,8,8,0,0,1-8-8Z"></path>
             </svg>
-            <span className="font-bold font-headline">ZikarXresham butt</span>
+            <span className="font-bold font-headline">ZikarX</span>
           </Link>
         </div>
 
